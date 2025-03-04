1d6e80972ef44ae0c8991a76c78c35bcd9f9d6b9
MISHAELNATTH <152916558+MISHAELNATTH@users.noreply.github.com>
2025-03-04 12:23:14 +0100

    Add files via upload

--- source_code.cpp
+++ source_code.cpp
@@ -2,6 +2,8 @@
 #include <string.h>
 #define MAX_CUSTOMERS 10000
 #define CUSTOMER_FILE "customers.txt"
+#define TABLE_FILE "Table.txt"
+#define MAX_TABLE 500
 
 // Structure to store customer details
 struct Customer{
@@ -41,6 +43,19 @@ void saveCustomersToFile(struct Customer *customers, int count) {
     fclose(file);
 }
 
+// Save Table to the file
+void saveTableFile(struct Table *table, int count) {
+    FILE *file = fopen(TABLE_FILE, "w");
+    if(!file){
+        printf("Error opening file for writing!\n");
+        return;
+    }
+    for(int i=0; i<count; i++) {
+        fprintf(file,"%d %d %s %d\n", table[i].tableID, table[i].capacity, table[i].name, table[i].Availablity);
+    }
+    fclose(file);
+}
+
 // Function to load customers from a file
 int loadCustomersFromFile(struct Customer *customers) {
     FILE *file = fopen(CUSTOMER_FILE, "r");
@@ -54,12 +69,26 @@ int loadCustomersFromFile(struct Customer *customers) {
     return count;
 }
 
+// Load Tables from a file
+int loadTable(struct Table *table) {
+    FILE *file = fopen(TABLE_FILE, "r");
+    if (!file){
+        return 0;
+    }
+    int count = 0;
+    while(fscanf(file,"%d %d %29s %d\n", &table[count].tableID, &table[count].capacity, table[count].name, &table[count].Availablity)==4){
+        count++;
+    }
+    fclose(file);
+    return count;
+}
+
 // Function to add a new customer
 void addCustomer(struct Customer *customers, int *count){
     if (*count>=MAX_CUSTOMERS){
         printf("Customer list is full!\n");
         return;
-    } 
+    }   
     struct Customer newCustomer;
     newCustomer.ID = (*count >0) ? customers[*count - 1].ID + 1 : 1;
     
@@ -76,6 +105,29 @@ void addCustomer(struct Customer *customers, int *count){
     (*count)++;
     saveCustomersToFile(customers, *count);
     printf("Customer added successfully!\n");
+}
+
+//ADD TABLES
+void addTable(struct Table *table, int *count){
+    if (*count>=MAX_TABLE){
+        printf("No more Space!!");
+        return;
+    } 
+    struct Table newTable;
+    newTable.tableID = (*count>0) ? table[*count - 1].tableID + 1 : 1;
+    
+    printf("Enter Table Capacity: ");
+    scanf("%d", &newTable.capacity);
+    
+    newTable.Availablity = 1;       // New tables are available by default
+    
+    printf("Enter Table Name: ");
+    scanf("%29s", newTable.name);
+    
+    table[*count] = newTable;
+    (*count)++;
+    saveTableFile(table, *count);
+    printf("Table added successfully!\n");
 }
 
 // Function to modify customer details
@@ -98,6 +150,25 @@ void modifyCustomer(struct Customer *customers, int count) {
         }
     }
     printf("Customer ID not found!\n");
+}
+
+void modifyTable(struct Table *table, int count) {
+    int T_ID;
+    printf("Enter Table ID to modify: ");
+    scanf("%d", &T_ID);
+    
+    for (int i = 0; i<count; i++){
+        if (table[i].tableID == T_ID){
+            printf("Enter new Capacity: ");
+            scanf("%d", &table[i].capacity);
+            printf("Enter new name: ");
+            scanf("%29s", table[i].name);
+            saveTableFile(table, count);
+            printf("Table details updated successfully!\n");
+            return;
+        }
+    }
+    printf("Table ID not found!\n");
 }
 
 // Function to display all customers in alphabetical order
@@ -123,6 +194,19 @@ void displayCustomers(struct Customer *customers, int count){
     }
 }
 
+// Function to display all tables
+void displayTables(struct Table *table, int count) {
+    if (count == 0) {
+        printf("No tables found!\n");
+        return;
+    }
+    
+    printf("Table List:\n");
+    for (int i = 0; i < count; i++) {
+        printf("ID: %d, Capacity: %d, Name: %s, Status: %s\n", table[i].tableID, table[i].capacity, table[i].name, table[i].Availablity ? "Available" : "Reserved");
+    }
+}
+
 // Function to delete a customer
 void deleteCustomer(struct Customer *customers, int *count){
     int ID;
@@ -142,39 +226,103 @@ void deleteCustomer(struct Customer *customers, int *count){
     }
     printf("Customer ID not found!\n");
 }
+
+// Function to delete a table
+void deleteTable(struct Table *table, int *count) {
+    int T_ID;
+    printf("Enter Table ID to delete: ");
+    scanf("%d", &T_ID);
     
+    for (int i=0; i<*count; i++) {
+        if (table[i].tableID == T_ID) {
+            for (int j=i; j<*count-1; j++) {
+                table[j] = table[j + 1];
+            }
+            (*count)--;
+            saveTableFile(table, *count);
+            printf("Table deleted successfully!\n");
+            return;
+        }
+    }
+    printf("Table ID not found!\n");
+}
 
 int main(){
     struct Customer customers[MAX_CUSTOMERS];
     int customerCount = loadCustomersFromFile(customers);
-    
-    int choice;
-    do {
-        printf("\nCustomer Management Menu:\n");
-        printf("1. Add Customer\n");
-        printf("2. Display Customers\n");
-        printf("3. Modify customer details.\n");
-        printf("4. Delete Customer\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        
-        if(choice==1){
-            addCustomer(customers, &customerCount);
-        }  
-        if(choice == 2){
-            displayCustomers(customers, customerCount);
-        }
-        if(choice==3){
-            modifyCustomer(customers, customerCount);
-        }
-        if(choice == 4){
-            deleteCustomer(customers, &customerCount);
-        }
-        if(choice==5){
-            printf("Exiting......\n");
-        }
-    }while(choice!=5);  
 
-    return 0;   
+    struct Table table[MAX_TABLE];
+    int TableCount = loadTable(table);
+
+    int menu_choice;
+    int choice;
+    int choice_T;
+
+    do{
+        printf("Enter 0 to exit\n");
+        printf("1. Customer Management Menu:\n");
+        printf("2. Table Management Menu:\n");
+        printf("Enter your Menu choice: ");
+        scanf("%d", &menu_choice);
+
+        if(menu_choice==1){
+            do {
+                printf("\nCustomer Management Menu:\n");
+                printf("1. Add Customer\n");
+                printf("2. Display Customers\n");
+                printf("3. Modify customer details.\n");
+                printf("4. Delete Customer\n");
+                printf("5. Exit\n");
+                printf("Enter your choice: ");
+                scanf("%d", &choice);
+                
+                if(choice==1){
+                    addCustomer(customers, &customerCount);
+                }  
+                if(choice == 2){
+                    displayCustomers(customers, customerCount);
+                }
+                if(choice==3){
+                    modifyCustomer(customers, customerCount);
+                }
+                if(choice == 4){
+                    deleteCustomer(customers, &customerCount);
+                }
+                if(choice==5){
+                    printf("Exiting......\n");
+                }
+            }while(choice!=5);  
+        }
+    
+        else if(menu_choice==2){
+            do {
+                printf("\nTable Management Menu:\n");
+                printf("1. Add Table\n");
+                printf("2. Display Table\n");
+                printf("3. Modify Table details\n");
+                printf("4. Remove Table\n");
+                printf("5. Exit\n");
+                printf("Enter your choice: ");
+                scanf("%d", &choice_T);
+                
+                if(choice_T==1){
+                    addTable(table, &TableCount);
+                }  
+                if(choice_T == 2){
+                    displayTables(table, TableCount);
+                }
+                if(choice_T==3){
+                    modifyTable(table, TableCount);
+                }
+                if(choice_T == 4){
+                    deleteTable(table, &TableCount);
+                }
+                if(choice_T==5){
+                    printf("Exiting......\n");
+                }
+            }while(choice_T!=5);  
+        
+            
+        }
+    }while(menu_choice!=0);
 }
